2b624ff914a351180a191717d4a83c7736f5262d
MikeMZK <mike.mazek@gmx.de>
2018-01-17 20:55:27 +0100

    xx

--- winThreadings/frmMain.Designer.cs
+++ winThreadings/frmMain.Designer.cs
@@ -28,20 +28,68 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnBeenden = new System.Windows.Forms.Button();
+            this.lblStoppuhr = new System.Windows.Forms.Label();
+            this.timerT = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 215);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(125, 34);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnBeenden
+            // 
+            this.btnBeenden.Location = new System.Drawing.Point(240, 215);
+            this.btnBeenden.Name = "btnBeenden";
+            this.btnBeenden.Size = new System.Drawing.Size(85, 34);
+            this.btnBeenden.TabIndex = 1;
+            this.btnBeenden.Text = "Beenden";
+            this.btnBeenden.UseVisualStyleBackColor = true;
+            this.btnBeenden.Click += new System.EventHandler(this.btnBeenden_Click);
+            // 
+            // lblStoppuhr
+            // 
+            this.lblStoppuhr.AutoSize = true;
+            this.lblStoppuhr.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStoppuhr.Location = new System.Drawing.Point(-4, 75);
+            this.lblStoppuhr.Name = "lblStoppuhr";
+            this.lblStoppuhr.Size = new System.Drawing.Size(347, 91);
+            this.lblStoppuhr.TabIndex = 2;
+            this.lblStoppuhr.Text = "00:00:00";
+            // 
+            // timerT
+            // 
+            this.timerT.Tick += new System.EventHandler(this.timerT_Tick);
             // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(337, 261);
+            this.Controls.Add(this.lblStoppuhr);
+            this.Controls.Add(this.btnBeenden);
+            this.Controls.Add(this.btnStart);
             this.Name = "frmMain";
-            this.Text = "Threads";
+            this.Text = "Stoppuhr";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnBeenden;
+        private System.Windows.Forms.Label lblStoppuhr;
+        private System.Windows.Forms.Timer timerT;
     }
 }
 
--- winThreadings/frmMain.cs
+++ winThreadings/frmMain.cs
@@ -6,15 +6,54 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace winThreadings
 {
     public partial class frmMain : Form
     {
+        Stopwatch sw = new Stopwatch();
+        string currenttime = "00:00:00";
+
         public frmMain()
         {
             InitializeComponent();
+            lblStoppuhr.Text = currenttime;
+            timerT.Interval = 1;
+        }
+
+        private void btnBeenden_Click(object sender, EventArgs e)
+        {
+            //this.Close();
+        }
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            Thread th = new Thread(ThreadMethod);
+            timerT.Enabled = true;
+            th.Start();
+        }
+
+        private void ThreadMethod()
+        {
+            sw.Start();
+            Thread.Sleep(2132);
+
+            TimeSpan ts = sw.Elapsed;
+
+            // Format and display the TimeSpan value.
+            string elapsedTime = String.Format("{0:00}:{1:00}.{2:00}",
+                ts.Minutes, ts.Seconds,
+                ts.Milliseconds / 10);
+            currenttime = elapsedTime;
+
+            sw.Stop();
+        }
+
+        private void timerT_Tick(object sender, EventArgs e)
+        {
+            lblStoppuhr.Text = currenttime;
         }
     }
 }
